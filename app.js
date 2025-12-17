27543b5a9a03cb0f8bf42e75fffb2b735bdea123
ahspence53 <147179766+ahspence53@users.noreply.github.com>
2025-12-17 19:17:08 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-<script src="script.js?v=1.0.2"></script>
+<script src="script.js?v=1.2.0"></script>
 /* ================= STORAGE ================= */
 let categories = JSON.parse(localStorage.getItem("categories")) || [];
 let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
