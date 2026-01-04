69eedc4814d59043680a26f54cd1617c3f718013
ahspence53 <147179766+ahspence53@users.noreply.github.com>
2026-01-04 14:42:02 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1161,25 +1161,7 @@ document.getElementById("cal-next").onclick = () => {
   renderCalendar();
 };
   
-function scheduleDiaryOpen(date) {
-  console.log("Scheduling diary for", date);
 
-  pendingDiaryDate = date;
-
-  if (diaryOpenScheduled) return;
-  diaryOpenScheduled = true;
-
-  setTimeout(() => {
-    console.log("Opening diary for", pendingDiaryDate);
-
-    diaryOpenScheduled = false;
-
-    if (pendingDiaryDate) {
-      openDiaryForDate(pendingDiaryDate);
-      pendingDiaryDate = null;
-    }
-  }, 150);
-}
 // ---- Open DB
 function openDiaryDB() {
   return new Promise((resolve) => {
